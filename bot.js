4c7de8758b800e7fab7e0057b138c0b7f8e10379
DaveSholy <48659370+DaveSholy@users.noreply.github.com>
2019-04-14 06:12:21 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`BOT`,"http://twitch.tv/S-F")
+client.user.setGame(`Bot Credit Soon`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
